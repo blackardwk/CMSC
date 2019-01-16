50cc08a39afb5ad8bb670b0c267bc45f02165b7c
blackardwk <46754191+blackardwk@users.noreply.github.com>
2019-01-16 10:34:13 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,27 @@
+/*******************************************************************
+ * Hello World
+ *******************************************************************
+ * Lab 1, first program
+ * _________________________________________________________________
+ * William Blackard
+ * 16 January 2019
+ * CMSC 255 003
+ *******************************************************************/
+ 
+ public class Hello {
+	public static void printHeading() {
+		System.out.println("Kip Blackard");
+		System.out.println("Hello World");
+		System.out.println("first program");
+		System.out.println("16 January 2019");
+		System.out.println("CMSC 255 003");
+		System.out.println();
+	}
+	
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+ }
+ 
+	
